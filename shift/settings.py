4a8d1f4b477d4eca9ac020b3a702918cf06a0033
2ndborn <micah.francis69@googlemail.com>
2024-03-20 14:31:09 +0000

    completed confirmation emails

--- shift/settings.py
+++ shift/settings.py
@@ -179,6 +179,7 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAUL_FROM_EMAIL = 'shift@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
