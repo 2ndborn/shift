1fd743d36e2464954053939fae5506502acdfc85
2ndborn <micah.francis69@googlemail.com>
2024-03-24 03:34:58 +0000

    remove secret key and set debug

--- shift/settings.py
+++ shift/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mp8s!7c68=h#i5%xq)n%=-%@2rzq3zwi&2y8@0hbn7tgwq)%l#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['2ndborn-shiftv1.herokuapp.com', 'localhost']
 
