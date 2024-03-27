aa0344042a91d2c71964770b92519fa843eaeec7
2ndborn <micah.francis69@googlemail.com>
2024-03-27 05:45:11 +0000

    change the code back to trouble shoot heroku deployment

--- shift/settings.py
+++ shift/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['2ndborn-shiftv1.herokuapp.', 'localhost']
+ALLOWED_HOSTS = ['2ndborn-shiftv1.herokuapp.com', 'localhost']
 
 
 # Application definition
