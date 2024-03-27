83f0a8ad4d5bd0142da9414b7b55f7c9f2bf01dc
2ndborn <micah.francis69@googlemail.com>
2024-03-27 06:08:31 +0000

    changed the app address on allowed hosts

--- shift/settings.py
+++ shift/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['2ndborn-shiftv1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['shiftv1-3ed21a1cd545.herokuapp.com', 'localhost']
 
 
 # Application definition
