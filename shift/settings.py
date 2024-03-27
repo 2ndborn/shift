9c3e28697b03f1631d51e257a2c0684abd5cb51f
2ndborn <micah.francis69@googlemail.com>
2024-03-27 06:41:20 +0000

    amended bucket name

--- shift/settings.py
+++ shift/settings.py
@@ -190,7 +190,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-vv1'
+    AWS_STORAGE_BUCKET_NAME = 'shiftv1'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
