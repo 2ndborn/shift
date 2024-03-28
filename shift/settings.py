beaff84d397eff72b0f98cf60cb7629b852494ad
2ndborn <micah.francis69@googlemail.com>
2024-03-28 15:12:14 +0000

    trouble shooting deployment to heroku

--- shift/settings.py
+++ shift/settings.py
@@ -125,17 +125,17 @@ WSGI_APPLICATION = 'shift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-# else:
-#     DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
