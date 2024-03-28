a042eff13585f47db0e5532bf186957254756424
2ndborn <micah.francis69@googlemail.com>
2024-03-28 18:59:47 +0000

    Updated the env.py file and settings

--- shift/settings.py
+++ shift/settings.py
@@ -180,6 +180,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+USE_AWS = os.getenv("USE_AWS") == "True"
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
