032fa5846472f59cefcb1c5a57e3e002340b840a
2ndborn <micah.francis69@googlemail.com>
2024-03-29 09:32:36 +0000

    updated setting

--- shift/settings.py
+++ shift/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://shift-v1-d8f667a6ca2b.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['shift-v1-d8f667a6ca2b.herokuapp.com','8000-2ndborn-shift-79eiaytri6.us1.codeanyapp.com']
 
 
 # Application definition
