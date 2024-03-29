35ea97c6d324864979a55d4d6c601b2baeceea58
2ndborn <micah.francis69@googlemail.com>
2024-03-29 16:51:15 +0000

    edited the margin botton on the post pages

--- community/views.py
+++ community/views.py
@@ -8,7 +8,7 @@ from .forms import PostForm, CommentForm
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all().order_by(-created_on)
+        posts = Post.objects.all().order_by(created_on)
 
         context = {
             'post_list': posts,
