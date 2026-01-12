61c06373e9fffd9efe4868c391ff665824a79c22
Micah Francis <micah.francis69@googlemail.com>
2026-01-12 08:07:39 +0000

    Add AllowIframeMiddleware for responsive testing

---
+++ home/middleware.py
@@ -0,0 +1,8 @@
+class AllowIframeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response.headers['X-Frame-Options'] = 'ALLOWALL'
+        return response
